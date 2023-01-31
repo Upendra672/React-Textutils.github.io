8a934ce208fccf72df9a06aff92a9b87b5580f2d
upendrakumar Yadav <yadavupendar400@gmail.com>
2023-02-01 01:45:35 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
-import Alert from './components/alert';
+// import Alert from './components/alert';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+
 
 function App() {
   const [Mode, setMode] = useState('light') //whether dark mode is enable or not
@@ -19,19 +21,26 @@ function App() {
   }
   }
   return (
-    <>
-    <div className=''>
+    <Router>
       <Navbar title="Textutils" mode={Mode} toggleMode={toggleMode}/>
-      <Alert alert="this is a alert"/>
+      {/* <Alert alert="this is a alert"/> */}
+
       <div className="container">
+      {/* <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
         <TextForm title="Enter the text to analyse:" mode={Mode}/>
-        {/* <About/> */}
+          </Route>
+        </Switch> */}
+        <Routes>
+          			<Route exact path="/" element={<TextForm title="Enter the text to analyse:" mode={Mode}/>} />
+          			<Route exact path="/about" element={<About />} />
+        		</Routes>
       </div>
-    </div>
-    
-    </>
-      
-  );
+    </Router>     
+  )
 }
 
-export default App;
+export default  App;
