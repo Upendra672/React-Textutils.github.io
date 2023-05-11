6360c869bcef7bf99df04fcee0f5e050f515607b
upendra672 <upendaryadav672@gmail.com>
2023-05-11 13:59:29 +0530

    button text fixing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar(props) {
           </ul>
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode === "light" ? "dark" : "light"} Mode</label>
           </div>
           {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
